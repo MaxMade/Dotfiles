c1697fef85d785b2ea5fb1b467fa88b1f130e622
Maximilian Ott <maximilian.o.ott@fau.de>
2021-09-18 12:00:19 +0200

    nvim: Add custom gitsigns

--- init.lua
+++ init.lua
@@ -182,7 +182,15 @@ vim.g['UltiSnipsJumpForwardTrigger'] = '<c-j>'
 vim.g['UltiSnipsJumpBackwardTrigger'] = '<c-k>'
 
 -- gitsigns
-require('gitsigns').setup()
+require('gitsigns').setup({
+	signs = {
+		add          = {hl = 'GitSignsAdd'   , text = '┃', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+		change       = {hl = 'GitSignsChange', text = '┇', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+		delete       = {hl = 'GitSignsDelete', text = '━', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+		topdelete    = {hl = 'GitSignsDelete', text = '━', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+		changedelete = {hl = 'GitSignsChange', text = '╍', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+	},
+})
 
 -- GruvBox
 vim.g['gruvbox_contrast_dark'] = 'hard'
