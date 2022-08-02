3e3cbcb9a5dc5e3ab997dac255e272ced1035c03
Maximilian Ott <maximilian.o.ott@fau.de>
2022-08-02 23:36:33 +0200

    dotfiles.py: Create symlink to kitty.conf

--- dotfiles.py
+++ dotfiles.py
@@ -21,7 +21,7 @@ dotfiles['tmux.conf'] = home + '/.tmux.conf'
 dotfiles['init.lua'] = home + '/.config/nvim/init.lua'
 dotfiles['zshrc'] = home + '/.zshrc'
 dotfiles['alacritty.yml'] = home + '/.alacritty.yml'
-dotfiles['kitty.conf'] = home + '/.config/kitty.conf'
+dotfiles['kitty.conf'] = home + '/.config/kitty/kitty.conf'
 
 
 def main() -> int:
