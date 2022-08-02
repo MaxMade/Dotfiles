f7045d5fae5a308d3fccae87e1a53436f1f874ee
Maximilian Ott <ott@cs.fau.de>
2022-08-02 22:56:36 +0200

    kitty: Add config

--- dotfiles.py
+++ dotfiles.py
@@ -21,6 +21,7 @@ dotfiles['tmux.conf'] = home + '/.tmux.conf'
 dotfiles['init.lua'] = home + '/.config/nvim/init.lua'
 dotfiles['zshrc'] = home + '/.zshrc'
 dotfiles['alacritty.yml'] = home + '/.alacritty.yml'
+dotfiles['kitty.conf'] = home + '/.config/kitty.conf'
 
 
 def main() -> int:
